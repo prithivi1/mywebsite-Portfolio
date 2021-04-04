b7b31d034994cd78d2e261c3999b59354247cc88
unknown <01prithivi@gmail.com>
2021-04-04 23:47:33 +0530

    first commit

---
+++ index_js.js
@@ -0,0 +1,29 @@
+		function callPage(){
+			var elmnt = document.getElementById("page");
+			elmnt.scrollIntoView();
+		}
+
+		function callHome(){
+			var elmnt = document.getElementById("home");
+			elmnt.scrollIntoView();
+		}
+
+		function callAbout(){
+			var elmnt = document.getElementById("about");
+			elmnt.scrollIntoView();
+		}
+
+		function callResume(){
+			var elmnt = document.getElementById("resume");
+			elmnt.scrollIntoView();
+		}
+
+		function callProject(){
+			var elmnt = document.getElementById("project");
+			elmnt.scrollIntoView();
+		}
+
+		function callContact(){
+			var elmnt = document.getElementById("contact");
+			elmnt.scrollIntoView();
+		}
